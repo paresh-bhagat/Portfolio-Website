353333e08e52b58c299cb900b665b8e4aca88973
Paresh Bhagat <pareshbhagat29@gmail.com>
2023-05-26 07:47:51 +0530

    Add typewriter effect and background image
    Signed-off-by: Paresh Bhagat <pareshbhagat29@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pareshbhagat
+
+container-fluid position-relative h-100 ps-3 px-lg-3 pt-lg-3 pb-lg-2
--- js/main.js
+++ js/main.js
@@ -4,3 +4,21 @@ $(function() {
 
     });
 });
+
+
+
+const text = document.querySelector(".typewritter-text");
+
+const textLoad = () => {
+        setTimeout(() => {
+            text.textContent = "I'm Paresh Bhagat";
+        }, 0);
+        setTimeout(() => {
+            text.textContent = "I'm a Software Engineer";
+        }, 4000);
+        setTimeout(() => {
+            text.textContent = "Nice to meet you";
+        }, 8000); //1s = 1000 milliseconds
+    }
+textLoad();
+setInterval(textLoad, 12000);
